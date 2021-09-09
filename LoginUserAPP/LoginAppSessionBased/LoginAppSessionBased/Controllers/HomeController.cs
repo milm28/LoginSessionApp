0cfbfcce570bcb139ab360f116fb319be0e86f68
milm28 <miroslav28@yahoo.com>
2021-09-09 10:47:14 +0200

    Aplikacija, list strana sa preko 15000 korisnika

---
+++ LoginUserAPP/LoginAppSessionBased/LoginAppSessionBased/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿
+using LoginAppSessionBased.Models.EFModels;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace LoginAppSessionBased.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            if(Session["User"] != null)
+            {
+                List<tblUserr> users = new List<tblUserr>();
+                for (int i = 0; i < 100000; i++)
+                {
+                    users.Add(new tblUserr
+                    {
+                     
+                        Name = "Name" + i.ToString(),
+                        UserName = "UserName" + i.ToString()
+
+
+                    });
+                }
+                using (LoginAppUsingSessionDBEntities db = new LoginAppUsingSessionDBEntities())
+                {
+                    db.tblUserrs.AddRange(users);
+                    db.SaveChanges();
+                }
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "Auth");
+            }
+            
+        }
+
+    }
+}
---
+++ LoginUserAPP/LoginAppSessionBased/LoginAppSessionBased/DTOs/LoginDTO.cs
@@ -0,0 +1,23 @@
+﻿
+//ovaj dto saljemo na view da bi mogli da se logujemo
+using System.ComponentModel.DataAnnotations;
+
+namespace LoginAppSessionBased.DTOs
+{
+    /// <summary>
+    /// Kalsa koja nam sluzi za logovanje na sistem
+    /// Nju saljemo na loginView i u nju popunjavamo podatke iz forme
+    /// </summary>
+    public class LoginDTO
+    {
+        #region Properties
+        [Required]
+        [StringLength(50,ErrorMessage ="Maximalan broj karaktera je 50")]
+        public string UserName { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Maximalan broj karaktera je 50")]
+        public string Password { get; set; }
+
+        #endregion
+    }
+}
